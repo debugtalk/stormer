a8d46651a7bd475efcd9830ff4ccd664ffaf57f5
Leo Lee <mail@debugtalk.com>
2017-03-01 21:54:32 +0800

    update overall script

--- examples/roogoo/overall.py
+++ examples/roogoo/overall.py
@@ -78,22 +78,6 @@ class UserBehavior(TaskSet):
 
         return validate_code
 
-    def login(self, account):
-        self.test_get_url('/api/v1/service_info?app_version=2.8.3&os_platform=ios')
-        self.send_validate_code(account)
-        self.test_get_url('/api/v1/my')
-        self.test_get_url('/api/v1/my/config')
-        self.test_get_url('/api/v1/my/config')
-        self.test_get_url('/api/v1/customized_recommendations?distance=0&gender=2&max_age=99999&min_age=0&online_time=4320&page=1&pagesize=20')
-        self.test_get_url('/api/v1/daily_contents?page=1&page_size=20')
-        self.test_get_url('/api/v1/notifications/unread?version=v2')
-        self.test_get_url('/api/v1/users/info')
-        self.test_get_url(url='/api/v1/punishments/banned_user_info?user_uuids%5B%5D={}'.format(account['uuid']), name='banned_user_info')
-        self.test_get_url('/api/v1/punishments')
-        self.test_get_url('/api/v1/multistage_tests/delta_questionnaires?detail=0')
-        self.test_get_url('/api/v1/black_house/my')
-        self.test_get_url('/api/v1/my/recommend_friends?distance=0&gender=2&max_age=200&min_age=0&online_time=4320&page=1')
-
     def logout(self):
         url = '/api/v1/users/logout'
         with self.client.post(url, data='', catch_response=True, **self.kwargs) as resp:
@@ -196,39 +180,59 @@ class UserBehavior(TaskSet):
             print("Register account data run out !!!")
             exit(0)
 
-    @task
-    def test_user_action(self):
+    @task(weight=116)
+    def test_api_v1_users(self):
         # 11.6k rpm
         self.test_get_url('/api/v1/users?id=&rongyun_id={}'.format(self.account['rongyun_id']), name='/api/v1/users')
 
+    @task(weight=77)
+    def test_api_v1_time_lines_list(self):
         # 7.75k rpm
         self.test_get_url('/api/v1/time_lines/list?page=1&page_size=20&type=messages&user_uuid={}'.format(self.account['id']), name='/api/v1/time_lines/list')
 
+    @task(weight=69)
+    def test_api_v1_customized_recommendations(self):
         # 6.95k rpm
         self.test_get_url('/api/v1/customized_recommendations?distance=0&gender=2&max_age=99999&min_age=0&online_time=4320&page=1&pagesize=20')
 
+    @task(weight=47)
+    def test_api_v1_notifications_unread(self):
         # 4.7k rpm
         self.test_get_url('/api/v1/notifications/unread?version=v2')
 
+    @task(weight=35)
+    def test_api_v1_punishments_banned_user_info(self):
         # 3.52k rpm
         self.test_get_url('/api/v1/punishments/banned_user_info?user_uuids%5B%5D={}'.format(self.account['id']), name='banned_user_info')
 
+    @task(weight=32)
+    def test_api_v1_my_follow_count(self):
         # 3.24k rpm
         self.test_get_url('/api/v1/my/follow_count')
 
+    @task(weight=27)
+    def test_api_v1_users_last_visited_at(self):
         # 2.7k rpm
         self.test_get_url('/api/v1/users/last_visited_at?rongyun_id={}'.format(self.account['rongyun_id']), name='last_visited_at')
 
+    @task(weight=23)
+    def test_api_v1_multistage_tests_questionnaire_packages(self):
         # 2.32k rpm
         self.test_get_url('/api/v1/multistage_tests/questionnaire_packages')
 
+    @task(weight=22)
+    def test_api_v1_users_tick(self):
         # 2.24k rpm
         self.client.post('/api/v1/users/tick', data='', **self.kwargs)
 
+    @task(weight=19)
+    def test_api_v1_my_k_tes_hint(self):
         # 1.88k rpm
         other_user_uuid = 'f4e676c2-b471-40fa-a828-e33439956833'
         self.test_get_url('/api/v1/my/k_tes_hint?other_user_uuid={}'.format(other_user_uuid), name='k_tes_hint')
 
+    @task(weight=18)
+    def test_api_v1_black_house_my(self):
         # 1.87k rpm
         self.test_get_url('/api/v1/black_house/my')
 
